3cd4a39af0f1ada95d573ba506f75aee672eae81
Krishnaveni-Raja <krishnaveniraja03@gmail.com>
2025-01-07 14:49:12 +0530

    Update signup.js

--- signup.js
+++ signup.js
@@ -32,8 +32,4 @@ signupButton.addEventListener('click', function(){
     }
 })
 
-var username= document.getElementById('username')
-var email= document.getElementById('email')
-var password= document.getElementById('password')
-
 
